8e3b4a6e777b5a24e8e28ad5a0aa2a63900534b9
Adam Grare <agrare@redhat.com>
2020-02-12 13:55:54 -0500

    Reduce the timout to open a connection to 10 sec
    If an attempt to connect to an Ansible Tower source hangs the default
    open_timeout of 60 seconds was starving the operations worker causing
    availability checks to appear to never complete.
    
    Relates to JIRA: TPINVTRY-851

--- lib/topological_inventory/ansible_tower/connection.rb
+++ lib/topological_inventory/ansible_tower/connection.rb
@@ -5,13 +5,14 @@ module TopologicalInventory::AnsibleTower
   class Connection
     include Logging
 
-    def connect(base_url, username, password, verify_ssl: ::OpenSSL::SSL::VERIFY_NONE)
+    def connect(base_url, username, password, verify_ssl: ::OpenSSL::SSL::VERIFY_NONE, open_timeout: 10)
       AnsibleTowerClient.logger = self.logger
       AnsibleTowerClient::Connection.new(
         :base_url   => api_url(base_url),
         :username   => username,
         :password   => password,
-        :verify_ssl => verify_ssl
+        :verify_ssl => verify_ssl,
+        :request    => {:open_timeout => open_timeout}
       )
     end
 
