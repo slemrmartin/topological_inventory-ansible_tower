a0cbd6ac3c9a74de28d1ecd56ba8094bcb637c54
Nick Carboni <ncarboni@redhat.com>
2020-03-25 11:45:59 -0400

    Add additional logging for availability checks
    https://projects.engineering.redhat.com/browse/TPINVTRY-839

--- lib/topological_inventory/ansible_tower/operations/source.rb
+++ lib/topological_inventory/ansible_tower/operations/source.rb
@@ -33,6 +33,7 @@ module TopologicalInventory
           rescue ::SourcesApiClient::ApiError => e
             logger.error("Failed to update Source id:#{source_id} - #{e.message}")
           end
+          logger.info("Source#availability_check completed: Source #{source_id} is #{source.availability_status}")
         end
 
         private
