0e7c6c5a426504fe811339b7e4b3884f53a35d93
Martin Slemr <mslemr@redhat.com>
2020-06-05 10:30:46 +0200

    Support for availability_status_error:nullable

--- spec/operations/source_spec.rb
+++ spec/operations/source_spec.rb
@@ -39,9 +39,7 @@ RSpec.describe(TopologicalInventory::AnsibleTower::Operations::Source) do
 
       # PATCH
       source_patch_body = {'availability_status' => described_class::STATUS_AVAILABLE}.to_json
-      # TODO: missing 'nullable: true' in availability_status_error
-      # endpoint_patch_body = {'availability_status' => described_class::STATUS_AVAILABLE, 'availability_status_error' => nil}.to_json
-      endpoint_patch_body = {'availability_status' => described_class::STATUS_AVAILABLE}.to_json
+      endpoint_patch_body = {'availability_status' => described_class::STATUS_AVAILABLE, 'availability_status_error' => nil}.to_json
 
       stub_patch(:source, source_patch_body)
       stub_patch(:endpoint, endpoint_patch_body)
